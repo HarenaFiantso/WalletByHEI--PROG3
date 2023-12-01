f1ef8c3b74559aba4bdef3e0d727fad74b9cfa5c
Fiantso Harena <fiantsorav@gmail.com>
2023-12-01 17:30:21 +0300

    feat: create SQL scripts for basics tables

---
+++ src/main/resources/create_table_transaction.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS transaction
+(
+    transaction_id   SERIAL PRIMARY KEY,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    transaction_type transaction_type NOT NULL,
+    amount           DOUBLE PRECISION NOT NULL,
+    label            VARCHAR(255)     NOT NULL,
+    account_id       INT              NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (account_id)
+);
---
+++ src/main/resources/index.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE wallet_by_hei;
+
+\c wallet_by_hei
+
+-- Creating currency table and types
+CREATE TYPE currency_name AS ENUM ('Euro', 'Ariary');
+CREATE TYPE currency_code AS ENUM ('EUR', 'MGA');
+CREATE TABLE IF NOT EXISTS currency
+(
+    currency_id   SERIAL PRIMARY KEY,
+    currency_name currency_name NOT NULL,
+    currency_code currency_code NOT NULL
+);
+
+-- Creating table account and type
+CREATE TYPE account_type AS ENUM ('BANK', 'CASH', 'MOBILE MONEY');
+CREATE TABLE IF NOT EXISTS account
+(
+    account_id   SERIAL PRIMARY KEY,
+    account_name VARCHAR(255) NOT NULL,
+    account_type account_type NOT NULL,
+    currency_id  INT          NOT NULL,
+    FOREIGN KEY (currency_id) REFERENCES currency (currency_id)
+);
+
+-- Creating tables transaction & category and type
+CREATE TYPE transaction_type AS ENUM ('CREDIT', 'DEBIT');
+CREATE TABLE IF NOT EXISTS transaction
+(
+    transaction_id   SERIAL PRIMARY KEY,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    transaction_type transaction_type NOT NULL,
+    amount           DOUBLE PRECISION NOT NULL,
+    label            VARCHAR(255)     NOT NULL,
+    account_id       INT              NOT NULL,
+    category_id      INT              NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (account_id)
+);
