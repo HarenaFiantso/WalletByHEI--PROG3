39843fe2bc2e451d257afdae82c612d2245dbeaf
Fiantso Harena <fiantsorav@gmail.com>
2023-12-02 10:15:11 +0300

    feat: initialize a java class to get a connection from database

---
+++ src/main/java/com/wallet/database/ConnectionToDb.java
@@ -0,0 +1,30 @@
+package com.wallet.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionToDb {
+  /* TODO: Put credentials to environment variable */
+
+  private static Connection connection;
+
+  public static Connection getConnection() {
+    try {
+      if (connection == null || connection.isClosed()) {
+        String DB_URL = System.getenv("DB_URL");
+        String DB_USERNAME = System.getenv("DB_USERNAME");
+        String DB_PASSWORD = System.getenv("DB_PASSWORD");
+
+        try {
+          connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+          throw new RuntimeException("Cannot connect to database : " + e.getMessage());
+        }
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException("Failed to get connection : " + e.getMessage());
+    }
+    return connection;
+  }
+}
