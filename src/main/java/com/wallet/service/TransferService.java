4fd94758c293f1eaf13a0e9ebf07c3e8dc59f815
Fiantso Harena <fiantsorav@gmail.com>
2023-12-07 13:02:18 +0300

    fix: review the logic of implementing a function that allow  to make a transfer between two accounts #2

--- src/main/java/com/wallet/service/TransferService.java
+++ src/main/java/com/wallet/service/TransferService.java
@@ -1,24 +1,27 @@
 package com.wallet.service;
 
-import com.wallet.model.Account;
-import com.wallet.model.Transaction;
-import com.wallet.model.TransferHistory;
+import com.wallet.model.*;
 import com.wallet.model.type.TransactionType;
 import com.wallet.repository.implementations.CurrencyCrudOperations;
 import com.wallet.repository.implementations.CurrencyValueCrudOperations;
 import com.wallet.repository.implementations.TransactionCrudOperations;
 import com.wallet.repository.implementations.TransferHistoryCrudOperations;
-
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 public class TransferService {
-  private final TransactionCrudOperations transactionCrudOperations = new TransactionCrudOperations();
+  private final CurrencyCrudOperations currencyCrudOperations = new CurrencyCrudOperations();
+  private final CurrencyValueCrudOperations currencyValueCrudOperations =
+      new CurrencyValueCrudOperations();
+  private final TransactionCrudOperations transactionCrudOperations =
+      new TransactionCrudOperations();
   private final TransferHistoryCrudOperations transferHistoryCrudOperations =
       new TransferHistoryCrudOperations();
 
-  /* TODO: Create a function to transfer money between two accounts (fourth question a) */
-  public void transferMoneyFirstCase(Account debitAccount, Account creditAccount, Double amount) throws IllegalAccessException {
+  /* TODO: Create a function to transfer money between two accounts (fourth question a)
+   *   - Should write a test for this method */
+  public void transferMoneyFirstCase(Account debitAccount, Account creditAccount, Double amount)
+      throws IllegalAccessException {
     if (debitAccount.getAccountId().equals(creditAccount.getAccountId())) {
       throw new IllegalAccessException("An account couldn't done transfer for himself");
     }
@@ -42,7 +45,8 @@ public class TransferService {
     transferHistoryCrudOperations.save(transferHistory);
   }
 
-  private Transaction createTransaction(Account account, Double amount, TransactionType transactionType) {
+  private Transaction createTransaction(
+      Account account, Double amount, TransactionType transactionType) {
     Transaction transaction = new Transaction();
     transaction.setTransactionDate(Timestamp.valueOf(LocalDateTime.now()));
     transaction.setTransactionType(transactionType);
@@ -50,5 +54,63 @@ public class TransferService {
     transaction.setAccountId(Math.toIntExact(account.getAccountId()));
 
     return transactionCrudOperations.save(transaction);
+  }
+
+  /* TODO: Create a function to transfer money between two accounts (fourth question b, in the case where accounts have
+  different currencies)
+  - Should write a test for this method */
+  public void transferMoneySecondCaseFirstPart(
+      Account debitAccount, Account creditAccount, Double amount) {
+    if (!isDifferentCurrency(debitAccount, creditAccount)) {
+      throw new IllegalArgumentException("Accounts should be different (EUR -> MGA) ");
+    }
+
+    Currency sourceCurrency = getCurrencyById((long) debitAccount.getCurrencyId());
+    Currency destinationCurrency = getCurrencyById((long) creditAccount.getCurrencyId());
+
+    if (!canConvertCurrencies()) {
+      throw new IllegalArgumentException("Couldn't do currency conversion (EUR -> MGA)");
+    }
+
+    Double exchangeRate = getExchangeRate(sourceCurrency, destinationCurrency);
+    Double convertedAmount = amount * exchangeRate;
+    Transaction debitTransaction =
+        createTransaction(debitAccount, convertedAmount, TransactionType.DEBIT);
+    Transaction creditTransaction =
+        createTransaction(creditAccount, convertedAmount, TransactionType.CREDIT);
+
+    /* Save transactions in the database */
+    transactionCrudOperations.save(debitTransaction);
+    transactionCrudOperations.save(creditTransaction);
+
+    /* Save transfer history in the database */
+    TransferHistory transferHistory = new TransferHistory();
+    transferHistory.setTransferDate(Timestamp.valueOf(LocalDateTime.now()));
+    transferHistory.setDebitTransactionId(Math.toIntExact(debitTransaction.getTransactionId()));
+    transferHistory.setCreditTransactionId(Math.toIntExact(creditTransaction.getTransactionId()));
+
+    transferHistoryCrudOperations.save(transferHistory);
+  }
+
+  private boolean isDifferentCurrency(Account account1, Account account2) {
+    return account1.getCurrencyId() != account2.getCurrencyId();
+  }
+
+  private Boolean canConvertCurrencies() {
+    /* TODO: If we want to allow conversion between EUR and MGA currencies, this method must be adjusted accordingly to
+    return true when conversion is possible, takes sourceCurrencyId and destinationCurrencyId as parameters */
+    return false;
+  }
+
+  private Currency getCurrencyById(Long currencyId) {
+    return currencyCrudOperations.findById(currencyId);
+  }
+
+  private Double getExchangeRate(Currency sourceCurrency, Currency destinationCurrency) {
+    CurrencyValue currencyValue =
+        currencyValueCrudOperations.findByCurrencies(
+            Math.toIntExact(sourceCurrency.getCurrencyId()),
+            Math.toIntExact(destinationCurrency.getCurrencyId()));
+    return currencyValue.getExchangeRate();
   }
 }
