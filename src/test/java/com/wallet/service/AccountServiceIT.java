ab7943efe1a52e01fd8f6785dc0f58df355d6a61
Fiantso Harena <fiantsorav@gmail.com>
2023-12-10 22:08:12 +0300

    refactor: reformat code

--- src/test/java/com/wallet/service/AccountServiceIT.java
+++ src/test/java/com/wallet/service/AccountServiceIT.java
@@ -1,16 +1,15 @@
 package com.wallet.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.wallet.model.Account;
 import com.wallet.model.Transaction;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class AccountServiceIT {
 
@@ -25,13 +24,15 @@ public class AccountServiceIT {
   void testGetBalanceAtDateTime() {
     List<Transaction> transactions = new ArrayList<>();
     Transaction creditTransaction = new Transaction();
-    creditTransaction.setTransactionDate(Timestamp.valueOf(LocalDateTime.parse("2023-01-01T12:00:00")));
+    creditTransaction.setTransactionDate(
+        Timestamp.valueOf(LocalDateTime.parse("2023-01-01T12:00:00")));
     creditTransaction.setTransactionType("CREDIT");
     creditTransaction.setAmount(100.0);
     transactions.add(creditTransaction);
 
     Transaction debitTransaction = new Transaction();
-    debitTransaction.setTransactionDate(Timestamp.valueOf(LocalDateTime.parse("2023-01-02T12:00:00")));
+    debitTransaction.setTransactionDate(
+        Timestamp.valueOf(LocalDateTime.parse("2023-01-02T12:00:00")));
     debitTransaction.setTransactionType("DEBIT");
     debitTransaction.setAmount(50.0);
     transactions.add(debitTransaction);
@@ -39,7 +40,8 @@ public class AccountServiceIT {
     Account account = new Account();
     account.setTransactionList(transactions);
 
-    double balance = accountService.getBalanceAtDateTime(account, LocalDateTime.parse("2023-01-03T12:00:00"));
+    double balance =
+        accountService.getBalanceAtDateTime(account, LocalDateTime.parse("2023-01-03T12:00:00"));
 
     assertEquals(50.0, balance);
   }
@@ -48,13 +50,15 @@ public class AccountServiceIT {
   void testGetBalanceHistoryInInterval() {
     List<Transaction> transactions = new ArrayList<>();
     Transaction creditTransaction = new Transaction();
-    creditTransaction.setTransactionDate(Timestamp.valueOf(LocalDateTime.parse("2023-01-01T12:00:00")));
+    creditTransaction.setTransactionDate(
+        Timestamp.valueOf(LocalDateTime.parse("2023-01-01T12:00:00")));
     creditTransaction.setTransactionType("CREDIT");
     creditTransaction.setAmount(100.0);
     transactions.add(creditTransaction);
 
     Transaction debitTransaction = new Transaction();
-    debitTransaction.setTransactionDate(Timestamp.valueOf(LocalDateTime.parse("2023-01-02T12:00:00")));
+    debitTransaction.setTransactionDate(
+        Timestamp.valueOf(LocalDateTime.parse("2023-01-02T12:00:00")));
     debitTransaction.setTransactionType("DEBIT");
     debitTransaction.setAmount(50.0);
     transactions.add(debitTransaction);
