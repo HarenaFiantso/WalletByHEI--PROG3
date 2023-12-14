e5e1ebf2f94583cde27199fdb3cf8cd3e7ee7669
Fiantso Harena <fiantsorav@gmail.com>
2023-12-14 19:56:00 +0300

    fix: update the index.sql file

--- src/main/resources/index.sql
+++ src/main/resources/index.sql
@@ -12,18 +12,6 @@ CREATE TABLE IF NOT EXISTS currency
     currency_code currency_code NOT NULL
 );
 
--- Creating currency value table
-CREATE TABLE IF NOT EXISTS currency_value
-(
-    currency_value_id       SERIAL PRIMARY KEY,
-    currency_value_date     DATE             NOT NULL DEFAULT CURRENT_DATE,
-    exchange_rate           DOUBLE PRECISION NOT NULL,
-    source_currency_id      INT              NOT NULL,
-    destination_currency_id INT              NOT NULL,
-    FOREIGN KEY (source_currency_id) REFERENCES currency (currency_id),
-    FOREIGN KEY (destination_currency_id) REFERENCES currency (currency_id)
-);
-
 -- Creating table account and type
 CREATE TYPE account_type AS ENUM ('BANK', 'CASH', 'MOBILE MONEY');
 CREATE TABLE IF NOT EXISTS account
@@ -33,6 +21,13 @@ CREATE TABLE IF NOT EXISTS account
     account_type account_type NOT NULL,
     currency_id  INT          NOT NULL,
     FOREIGN KEY (currency_id) REFERENCES currency (currency_id)
+);
+
+-- Creating category table
+CREATE TABLE IF NOT EXISTS category
+(
+    category_id   SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
 );
 
 -- Creating tables transaction & category and type
@@ -48,13 +43,3 @@ CREATE TABLE IF NOT EXISTS transaction
     FOREIGN KEY (account_id) REFERENCES account (account_id)
 );
 
--- Creating tables transfer history
-CREATE TABLE IF NOT EXISTS transfer_history
-(
-    transfer_history_id   SERIAL PRIMARY KEY,
-    transfer_date         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    debit_transaction_id  INT       NOT NULL,
-    credit_transaction_id INT       NOT NULL,
-    FOREIGN KEY (debit_transaction_id) REFERENCES transaction (transaction_id),
-    FOREIGN KEY (credit_transaction_id) REFERENCES transaction (transaction_id)
-);
